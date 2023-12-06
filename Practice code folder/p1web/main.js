2b56333483b1b97eeb5d30bb0fa08c1114ef7332
SAURABH TIWARI <115872589+SAURABHTIWARIVBSPU@users.noreply.github.com>
2023-12-06 20:52:33 +0530

    succes

---
+++ Practice code folder/p1web/main.js
@@ -0,0 +1,6 @@
+function openMenu() {
+    // Add your menu opening logic here
+    console.log('Menu opened!');
+    // You can replace the above line with the actual logic you want to execute when the menu is opened.
+  }
+  
